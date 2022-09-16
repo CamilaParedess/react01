aad4d8c822128b0ac97298055df1ad17dcf12611
CamilaParedess <cami-antonella09@hotmail.com>
2022-09-15 21:15:05 -0300

    desafio-maps-promises

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-      <ItemListContainer />
+        <ItemListContainer />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,29 @@
-import './NavBar.style.css'
-import logo from '../../logo.svg'
-import CartWidget from '../CartWidget/CartWidget'
+import "./NavBar.style.css";
+import logo from "../../logo.svg";
+import CartWidget from "../CartWidget/CartWidget";
 
-function NavBar () {
-        return (
-        <div className='NavBar-container'>
-            <a href='/'>
-            <img src={logo} className="NavBar-logo" alt="logo"/>
-            </a>
-            <h1>Nombre de la página</h1>
-            <div className='NavBar-navLinks'>
-                <ul>
-                    <li>
-                        <a href='/'>Inicio</a>
-                    </li>
-                    <li>
-                        <a href='/'>Productos</a>
-                    </li>
-                    <li>
-                        <a href='/'>Sobre nosotros</a>
-                    </li>
-                </ul>
-                < CartWidget />
-            </div>
-            
+function NavBar() {
+    return (
+        <div className="NavBar-container">
+          <a href="/">
+            <img src={logo} className="NavBar-logo" alt="logo" />
+          </a>
+          <div className="NavBar-navLinks">
+            <ul>
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/">Productos</a>
+              </li>
+              <li>
+                <a href="/">Nosotros</a>
+              </li>
+            </ul>
+          </div>
+          <CartWidget />
         </div>
-        );
+      );
 }
 
 export default NavBar;
